0587f1f8fc5cbe1296cd6f012dfbf0466cf6e607
mklau <matthewklau@fas.harvard.edu>
2016-05-26 12:06:11 -0400

    build.R now exists to create the notes in html and pdf as well as an ioslides talk

---
+++ doc/notes/build.R
@@ -0,0 +1,14 @@
+### Builds the course notes and presentation.
+## 
+## It can be run from the command line using
+## Rscript build.R
+
+library(rmarkdown)
+## Build notes html
+render(tail(commandArgs(trailingOnly=TRUE)),output_format='html_document')
+## Build notes pdf
+render(tail(commandArgs(trailingOnly=TRUE)),output_format='pdf_document')
+## Build presentation html
+x <- tail(commandArgs(trailingOnly=TRUE))
+render(x,output_format='ioslides_presentation',output_file=paste('talk_',sub('.Rmd','',x,ignore.case=TRUE),'.html',sep=''))
+
