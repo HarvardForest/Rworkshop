1688111afc5c4fe8989a6f7b207aaaa34553dec3
mklau <matthewklau@fas.harvard.edu>
2016-06-02 15:44:38 -0400

    final notes day 1 forwarded to manisha

--- doc/notes/build.R
+++ doc/notes/build.R
@@ -6,6 +6,8 @@
 ## 
 
 library(rmarkdown)
+library(knitr)
+
 ## Build notes html
 render(tail(commandArgs(trailingOnly=TRUE)),output_format='html_document')
 ## Build notes pdf
@@ -13,4 +15,5 @@ render(tail(commandArgs(trailingOnly=TRUE)),output_format='pdf_document')
 ## Build presentation html
 x <- tail(commandArgs(trailingOnly=TRUE))
 render(x,output_format='ioslides_presentation',output_file=paste(sub('.Rmd','',x,ignore.case=TRUE),'_talk.html',sep=''))
-
+## separate code 
+#purl(input=x,output=paste(sub('.Rmd','',x,ignore.case=TRUE),'_rcode.R',sep=''))
