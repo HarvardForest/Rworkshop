350de879e6872fc10b42fe35bd804f819ddbb15f
mklau <matthewklau@fas.harvard.edu>
2016-06-16 18:23:45 -0400

    fixed file path issues and rebuilt docs

--- doc/notes/build.R
+++ doc/notes/build.R
@@ -9,9 +9,11 @@ library(rmarkdown)
 library(knitr)
 
 ## Build notes html
-render(tail(commandArgs(trailingOnly=TRUE)),output_format='html_document')
+x <- tail(commandArgs(trailingOnly=TRUE))
+render(x,output_format='html_document')
 ## Build notes pdf
-render(tail(commandArgs(trailingOnly=TRUE)),output_format='pdf_document')
+x <- tail(commandArgs(trailingOnly=TRUE))
+render(x,output_format='pdf_document')
 ## Build presentation html
 x <- tail(commandArgs(trailingOnly=TRUE))
 render(x,output_format='ioslides_presentation',output_file=paste(sub('.Rmd','',x,ignore.case=TRUE),'_talk.html',sep=''))
