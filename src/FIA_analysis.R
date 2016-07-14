0f7d5e48832dcf424aef1664a1258ef4f9a4ad41
mklau <matthewklau@fas.harvard.edu>
2016-07-14 15:58:21 -0400

    fixed ggplotting

--- src/FIA_analysis.R
+++ src/FIA_analysis.R
@@ -1,7 +1,9 @@
 library(plyr)
 library(ggplot2)
+
 ##this is for the instructors so that we have proof of concept when we're asking them to do things
-setwd("D:/Rworkshop/Rworkshop/data/")
+#setwd("D:/Rworkshop/Rworkshop/data/")
+#setwd('..data')
 MAtrees <- read.csv("CleanedMATrees.csv")
 WAtrees <- read.csv("CleanedWATrees.csv")
 
@@ -16,11 +18,16 @@ ddply(MAtrees,.(INVYR),summarise,TotalCarbon = sum(CARBON_AG,na.rm=T),CarbPerSqM
 ddply(WAtrees,.(INVYR),summarise,TotalCarbon = sum(CARBON_AG,na.rm=T),CarbPerSqMi = TotalCarbon / WAsqmi, CarbPerSqMiFor = TotalCarbon/(WAsqmi * WApercFor))
 
 ### plot values
-pdf('../results/totCperYr.pdf')
-# Same, but with different colors and add regression lines
 dat <- rbind(MAtrees,WAtrees)
 dat$state <- c(rep('MA',nrow(MAtrees)),rep('WA',nrow(WAtrees)))
-ggplot(dat, aes(x=INVYR, y=DIA, color=state)) + geom_point(shape=1) + scale_colour_hue(l=50) + # Use a slightly darker palette than normal
-    geom_smooth(method=lm,   # Add linear regression lines
-                se=TRUE)    # Don't add shaded confidence region
+
+pdf('../results/totCperYr.pdf')
+
+ggplot(dat, aes(x=CN, y=PLT_CN, color=state)) + 
+    geom_point(shape=1) + 
+        scale_colour_hue(l=50) + 
+            geom_smooth(method=lm, se=TRUE) 
+
 dev.off()
+
+
